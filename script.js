c0feea1f108ba6629e2f8b761b84b0826756e844
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-05-24 00:11:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function threeSum(arr, target) {
+function threeSum(decimal) {
 // write your code here
-  
+	let ans="";
+	for(let i=0;decimal>=0;i++)
+		{
+			let rem=decimal%2;
+			midans=midans+${rem};
+		}
+	let revans="";
+
+	for(let j=ans.length-1;j>=0;j++)
+		{
+			revans +=ans[j];
+		}
+  console.log(revans);
 }
 
 module.exports = threeSum;
